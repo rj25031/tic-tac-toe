786c7eccefce1516d0d24d1815652cdce37cc5d1
Rupesh <rj25031@gmail.com>
2023-07-07 17:26:26 +0530

    v 2.0.0

--- script.js
+++ script.js
@@ -1,93 +1,141 @@
+const Boxes = Array.from(document.getElementsByClassName("box"));
+const button = document.getElementById('btn');
+const player1 = document.getElementsByClassName('players')[0]
+const player2 = document.getElementsByClassName('players')[1]
+const pw1=document.getElementsByClassName('winner')[0]
+const pw2=document.getElementsByClassName('lose')[0]
+const pt=document.getElementsByClassName('tied')[0]
+const pc1=document.getElementById('ymove')
+const pc2=document.getElementById('omove')
+const ps1 = 'X';
+const ps2 = 'O';
+var currentMove = true;
+var move = 0;
+var tie = true;
+var pwinCount1=0;
+var pwinCount2=0;
+const winCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+]
 
-let a = document.getElementsByClassName("box")
-var p=true
-var cli=0
-var ev=(event)=>{
-    var t = event.target  
-    cli+=1
-    tie_check(cli) 
-    square(t)
-   }
-for(let e of a){
-    e.addEventListener("click",ev)     
+
+
+Boxes.forEach(element => {
+    element.addEventListener('click', clickFunction)
+});
+
+
+
+function clickFunction(t) {
+    const target = t.target;
+    move += 1;
+    addMove(target);
+    win();
+    tieCheck();
+
 }
 
-let p1=document.getElementById("player1")
-let p2=document.getElementById("player2")
 
-
-function square(sq){
-    sq.removeEventListener('click',ev)
-if(p==true){
-
-    sq.innerHTML="X"
-    p2.classList.toggle("back")
-    p1.classList.toggle("back")
-    p=false
-}else{
-    sq.innerHTML="O"
-    p1.classList.toggle("back")
-    p2.classList.toggle("back")
-    p=true
- }
-res1(0,1,2)
-res1(3,4,5)
-res1(6,7,8)
-res1(0,4,8)
-res1(2,4,6)
-res1(0,3,6)
-res1(1,4,7)
-res1(2,5,8)
-
-res2(0,1,2)
-res2(3,4,5)
-res2(6,7,8)
-res2(0,4,8)
-res2(2,4,6)
-res2(0,3,6)
-res2(1,4,7)
-res2(2,5,8)
-}
-
-function res1(i,j,k){
-    if(a[i].innerHTML=="X" && a[j].innerHTML=="X" && a[k].innerHTML=="X"){
-        setTimeout(()=>{
-            document.body.innerHTML="<h1><b>Player 1 Won</b></h1>"
-         },200)
-         
-
-        setTimeout(()=>{
-           location.reload()
-        },2000)
+function addMove(target) {
+    if (currentMove) {
         
+        target.innerHTML = ps1;
+        currentMove = !currentMove
+        player1.classList.toggle('back')
+        player2.classList.toggle('back')
+    } else {
+        target.innerHTML = ps2;
+        currentMove = !currentMove
+        player1.classList.toggle('back')
+        player2.classList.toggle('back')
+    }
+    target.classList.add('disable')
+}
+
+
+function win() {
+    for (let e of winCombination) {
+        let [i, j, k] = e
+        if (Boxes[i].innerHTML == ps1 && Boxes[j].innerHTML == ps1 && Boxes[k].innerHTML == ps1) {
+            winColor(i, j, k);
+            removeClick();
+            tie = false;
+            pw1.classList.remove('none');
+            pwinCount1++;
+            pc1.innerHTML=` Player 1 : ${pwinCount1}`;
+            reset();
+        }
+        if (Boxes[i].innerHTML == ps2 && Boxes[j].innerHTML == ps2 && Boxes[k].innerHTML == ps2) {
+            winColor(i, j, k);
+            removeClick();
+            tie = false;
+            pw2.classList.remove('none');
+            pwinCount2++;
+            pc2.innerHTML=` Player 2 : ${pwinCount2}`;
+            reset();
+        }
     }
 }
 
-function res2(i,j,k){
-    if(a[i].innerHTML=="O" && a[j].innerHTML=="O" && a[k].innerHTML=="O"){
-        setTimeout(()=>{
-            document.body.innerHTML="<h1><b>Player 2 Won</b></h1>"
-         },200)
-         
 
-        setTimeout(()=>{
-           location.reload()
-        },2000)
-        
+function removeClick() {
+
+    Boxes.forEach(ele => {
+        ele.removeEventListener('click', clickFunction)
+    })
+
+}
+
+
+function winColor(i, j, k) {
+    Boxes[i].classList.add('win')
+    Boxes[j].classList.add('win')
+    Boxes[k].classList.add('win')
+
+}
+
+function reset() {
+    button.addEventListener('click', resetHandler);
+}
+
+
+function resetHandler() {
+    button.removeEventListener('click', resetHandler);
+    currentMove = true;
+    move = 0;
+    tie = true;
+    pw1.classList.add('none');
+    pw2.classList.add('none');
+    player1.classList.add('back')
+    player2.classList.remove('back')
+    pt.classList.add('none')
+    Boxes.forEach(element => {
+        element.innerHTML = '';
+        element.addEventListener('click', clickFunction)
+        element.classList.remove('disable')
+        element.classList.remove('tie')
+        element.classList.remove('win')
+    });
+}
+
+
+function tieCheck() {
+    if (move == 9 && tie == true) {
+        tieColor();
+        pt.classList.remove('none')
+        reset();
     }
 }
 
-function tie_check(c){
-    if(c==9){
-        setTimeout(()=>{
-            document.body.innerHTML="<h1><b>Tie</b></h1>"
-         },200)
-         
-
-        setTimeout(()=>{
-           location.reload()
-        },2000)
-        
-    }
+function tieColor() {
+    Boxes.forEach(ele => {
+        ele.classList.add('tie')
+    })
 }
- 
